f20f313df03feea0a2188b7797cda6b97d1f068f
堀田凌太郎 <hotta@hottaryoutarounoMacBook-Pro.local>
2025-03-22 23:38:06 +0900

    不要なコメント削除/使用していないファイルの削除

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,24 +1,23 @@
-# spec/requests/products_show_spec.rb
 require 'rails_helper'
 
 RSpec.describe "Products Show Page", type: :request do
-  let!(:product) { create(:product) }  # FactoryBotを使って製品データを作成
+  let!(:product) { create(:product) }
   
   describe "GET /products/:id" do
     before do
-      get product_path(product.id) # 製品ページをリクエスト
+      get product_path(product.id)
     end
 
     it "returns a successful response" do
-      expect(response).to have_http_status(200) # レスポンスが成功か確認
+      expect(response).to have_http_status(200)
     end
 
     it "displays the product name" do
-      expect(response.body).to include(product.name) # 商品名が表示されているか確認
+      expect(response.body).to include(product.name)
     end
 
     it "displays the product price" do
-      expect(response.body).to include(product.price.to_s) # 商品価格が表示されているか確認
+      expect(response.body).to include(product.price.to_s)
     end
   end
 end
