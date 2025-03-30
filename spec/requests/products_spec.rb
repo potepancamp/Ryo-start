9bccd7daeb512e93863c67728534424d4233efb8
堀田凌太郎 <hotta@hottaryoutarounoMacBook-Pro.local>
2025-03-30 12:58:40 +0900

    gitignore/rubocop/500エラーの修正

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Products Show Page", type: :request do
   let!(:product) { create(:product) }
-  
   describe "GET /products/:id" do
     before do
       get product_path(product.id)
