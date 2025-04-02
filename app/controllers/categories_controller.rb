5877cad9d286d1be9123116fed432dcc151f4bb9
堀田凌太郎 <hotta@hottaryoutarounoMacBook-Pro.local>
2025-04-03 00:50:08 +0900

    進捗1

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,6 @@
 class CategoriesController < ApplicationController
   def show
-    @taxon = Spree::Taxon.find(params[:taxon_id])
-    @taxonomies = Spree::Taxonomy.includes(:root)
-    @taxon_products = @taxon.products
-    @products = Spree::Product.includes(master: [:default_price, images: [attachment_attachment: [:blob]]]).in_taxons(@taxon.name)
-    @parent_taxons = @taxon.ancestors
-
-    add_breadcrumb 'Home', :root_path
-    add_breadcrumb @taxon.name, category_path(@taxon)  
+      @taxon = Spree::Taxon.find(params[:taxon_id])
+      @products = Spree::Product.includes(master: [:images, :default_price]).in_taxon(@taxon)
   end
 end
