b2aa38290fad4d0f17c6d7386e9b013c34b9940a
堀田凌太郎 <hotta@hottaryoutarounoMacBook-Pro.local>
2025-04-09 00:12:21 +0900

    カテゴリーページの実装

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "Products Show Page", type: :request do
-  let(:product) { create(:product) }
+  let(:product) do
+    taxonomy = create(:taxonomy)
+    parent_taxon = create(:taxon, name: "親カテゴリ", taxonomy: taxonomy)
+    child_taxon = create(:taxon, name: "子カテゴリ", taxonomy: taxonomy, parent: parent_taxon)
+
+    product = create(:product)
+    product.taxons << child_taxon
+
+    product
+  end
+
   describe "GET /products/:id" do
     before do
       get product_path(product.id)
@@ -17,6 +27,11 @@ RSpec.describe "Products Show Page", type: :request do
 
     it "displays the product price" do
       expect(response.body).to include(product.price.to_s)
+    end
+
+    it "displays breadcrumbs with correct category names" do
+      expect(response.body).to include("親カテゴリ")
+      expect(response.body).to include("子カテゴリ")
     end
   end
 end
