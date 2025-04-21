b3bb9c1733d2dfb6114484e43e858e6da932a986
堀田凌太郎 <hotta@bitsense-iMacT08.local>
2025-04-21 19:56:08 +0900

    修正進捗

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,22 +2,23 @@ class CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:taxon_id])
     @products = Spree::Product.includes(master: [:images, :default_price]).in_taxon(@taxon)
-
-    @breadcrumbs = build_breadcrumbs(@taxon)
+    
+    # `parent_id` を利用して最上位タクソンを取得
+    @ancestors = get_ancestors(@taxon)
   end
 
   private
 
-  def build_breadcrumbs(taxon)
-    breadcrumbs = [{ name: 'ホーム', path: root_path }]
+  def get_ancestors(taxon)
+    ancestors = []
 
-    taxon.
-      self_and_ancestors.
-      where.not(name: %w(Categories Brand)).
-      each do |t|
-        breadcrumbs << { name: t.name, path: category_path(t.id) }
-      end
+    # 現在のタクソンを親タクソンがない最上位タクソンまでたどる
+    while taxon.parent_id.present?
+      ancestors.unshift(taxon)
+      taxon = Spree::Taxon.find(taxon.parent_id)
+    end
+    ancestors.unshift(taxon)  # 最上位タクソンも追加
 
-    breadcrumbs
+    ancestors
   end
 end
